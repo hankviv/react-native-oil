7f672cbf5c2ed478e1f84318b639685fe177b68b
hankdev <hankviv@qq.com>
2018-07-02 15:28:47 +0800

    day6

--- js/page/oilCost/CheckOilRecord.js
+++ js/page/oilCost/CheckOilRecord.js
@@ -5,6 +5,7 @@ import {
     View,
     Text,
     Button,
+    ListView,
 } from 'react-native';
 import DatePicker from 'react-native-datepicker'
 import moment from 'moment';
@@ -13,31 +14,48 @@ import DataRepository from '../../data/DataRepository'
 export default class CheckOilRecord extends Component{
     constructor(props){
         super(props);
-        let Handle = new DataRepository();
-
-        let date = moment().format('YYYY-MM-DD HH:mm');
-        let localData = Handle.getData('oilRecord',(value)=>{
-            alert(value);
-        })
-
-        this.state = {date:date}
+        Handle = new DataRepository();
+        ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.state = {
+            'Handle':Handle,
+            'dataShow':false,
+        }
+        this._loadData();
     }
 
-
+    _loadData()
+    {
+        Handle.getData('oilRecord',(result)=>{
+            if(result){
+                //alert(result);
+                this.setState({dataSource: ds.cloneWithRows(JSON.parse(result)),'dataShow':true});
+            }
+        })
+    }
+    
     render() {
         return (
             <View style={styles.container}>
                 <Text style={{fontSize:20,color:'red'}}>AddRecord</Text>
-
                 <Text style={styles.text} onPress={() =>
                     this.props.navigation.goBack()
                 }>save</Text>
                 <Text style={styles.text} onPress={() =>
                     this.props.navigation.goBack()
-                }></Text>
+                }>goback</Text>
+                {
+                   this.state.dataShow ?
+                       <ListView
+                           dataSource={this.state.dataSource}
+                           renderRow={(rowData) => <Text>{JSON.stringify(rowData)}</Text>}
+                        />
+                        : <Text>loading</Text>
+                }
+
             </View>
         );
     }
+
 }
 
 const styles = StyleSheet.create({
