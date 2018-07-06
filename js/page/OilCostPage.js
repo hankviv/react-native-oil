378a6f2937b05915a8c2e7223e14d780b53c09bf
hankdev <hankviv@qq.com>
2018-07-06 19:07:48 +0800

    day10:oilCost

--- js/page/OilCostPage.js
+++ js/page/OilCostPage.js
@@ -4,24 +4,59 @@ import {
     StyleSheet,
     View,
     Text,
+    TouchableOpacity,
+    Image,
+    ScrollView,
 } from 'react-native';
+import NavigationBar from '../common/NavigationBar';
 
 export default class OilCostPage extends Component{
     constructor(props) {
         super(props);
-        //alert(JSON.stringify(this.props));
+    }
+
+    renderLeftButton(image){
+        return <TouchableOpacity
+            style={{padding: 8}}
+            onPress={()=>{
+                this.props.navigation.navigate('CheckOilRecord');
+            }}>
+            <Image
+                style={{width: 50, height: 50}}
+                source={image}/>
+        </TouchableOpacity>;
+    }
+
+    renderRightButton(image){
+        return <TouchableOpacity
+            style={{padding: 8}}
+            onPress={()=>{
+                this.props.navigation.navigate('AddOilRecord');
+            }}>
+            <Image
+                style={{width: 50, height: 50}}
+                source={image}/>
+        </TouchableOpacity>;
     }
 
     render(){
         return(
-            <View style={styles.container}>
-                <Text style={styles.text} onPress={()=>
-                    this.props.navigation.navigate('CheckOilRecord')
-                }>CheckOilRecord</Text>
-                <Text style={styles.text} onPress={()=>
-                    this.props.navigation.navigate('AddOilRecord')
-                }>AddOilRecord</Text>
-                <Text style={styles.text}>OilCost</Text>
+            <View style={{backgroundColor:'#FFFFFF'}}>
+
+
+                <View>
+                    <ScrollView>
+                        <Text style={styles.showBanner}>OilCost</Text>
+                        <Text style={styles.showBanner}>OilCost</Text>
+                        <Text style={styles.showBanner}>OilCost</Text>
+                        <Text style={styles.showBanner}>OilCost</Text>
+                        <Text style={styles.showBanner}>OilCost</Text>
+                        <Text style={styles.showBanner}>OilCost</Text>
+                        <Text style={styles.showBanner}>OilCost</Text>
+                    </ScrollView>
+                </View>
+
+
             </View>
         );
     }
@@ -32,11 +67,46 @@ export default class OilCostPage extends Component{
 const styles = StyleSheet.create({
     container:{
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
+    },
+    NavigationBar:{
+        backgroundColor:'#FFFFFF',
+        borderColor:'#E5E5E5',
+        borderBottomWidth:1
+    },
+    showBanner:{
+      backgroundColor:'#2A0D31',
+      height:100,
+        width:200,
+    },
+    scrollView:{
+        flex: 1,
+    },
+    item:{
+        marginTop:12,
+        paddingBottom:6,
+        borderColor:'#E5E5E5',
+        borderBottomWidth:1,
     },
     text:{
-        marginTop:10,
-        fontSize:20,
+        fontSize:12
+    },
+    textInput:{
+        marginTop:2,
+        height: 50,
+        fontSize:20
+    },
+    changeMoney:{
+        flex: 1,
+        flexDirection:'row',
+        justifyContent:'center',
+        marginTop:12,
+        paddingBottom:6,
+        borderColor:'#E5E5E5',
+        borderBottomWidth:1,
+    },
+    changeMoneyItem:{
+        flex: 1,
+        flexDirection:'row',
+        alignItems:'center'
     }
 })
